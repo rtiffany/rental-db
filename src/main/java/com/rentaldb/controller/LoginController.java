3b8b5ff45b13d9ac1feaf253e6e65595604505c3
ryan <ryan@ryan>
2022-12-06 00:37:29 -0500

    added LoginController

--- src/main/java/com/rentaldb/controller/LoginController.java
+++ src/main/java/com/rentaldb/controller/LoginController.java
@@ -1,14 +1,28 @@
 package com.rentaldb.controller;
 
+import com.rentaldb.mainApp.rentaldb;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class LoginController {
     @FXML
-    private Label welcomeText;
+    private Button loginButton;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onHelloButtonClick(ActionEvent event) throws IOException {
+        //loginButton.setText("click");
+        System.out.println("test");
+        Parent loginParent = FXMLLoader.load(rentaldb.class.getResource("Equipment-view.fxml"));
+        Scene testScene = new Scene(loginParent);
+
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(testScene);
     }
 }
