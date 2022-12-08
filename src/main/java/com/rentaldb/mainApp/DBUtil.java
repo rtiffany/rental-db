744b19dfeb11ca1d762d44e19e17ee9817f56cd1
ryan <ryan@ryan>
2022-12-08 09:43:11 -0500

    added jdbc library and skeleton class

--- src/main/java/com/rentaldb/mainApp/DBUtil.java
+++ src/main/java/com/rentaldb/mainApp/DBUtil.java
@@ -1,4 +1,31 @@
 package com.rentaldb.mainApp;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DBUtil {
+
+    private String DATABASE_URL;
+    private String DATABASE_USERNAME;
+    private String DATABASE_PASSWORD;
+
+    public DBUtil(String url, String username, String password) {
+        this.DATABASE_URL = url;
+        this.DATABASE_USERNAME = username;
+        this.DATABASE_PASSWORD = password;
+    }
+
+    public void test() {
+        String query = "SELECT fname, lname FROM Employee";
+        try {
+            Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
+            Statement statement = connection.createStatement();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+    }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.rentaldb {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires java.sql;
+
+
     opens com.rentaldb.mainApp to javafx.fxml;
     exports com.rentaldb.mainApp;
     exports com.rentaldb.controller;
