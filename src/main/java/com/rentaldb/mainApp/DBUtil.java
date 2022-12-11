746bfc99c7dc2dcdf3f0e0d10f53ae224f322565
ryan <ryan@ryan>
2022-12-11 18:07:39 -0500

    got connection working from login

--- src/main/java/com/rentaldb/mainApp/DBUtil.java
+++ src/main/java/com/rentaldb/mainApp/DBUtil.java
@@ -7,29 +7,36 @@ import java.sql.Statement;
 
 public class DBUtil {
 
-    private String DATABASE_URL;
+    private static final DBUtil instance = new DBUtil();
+
+    private static Connection connection = null;
+
+    private String DATABASE_HOSTNAME;
     private String DATABASE_USERNAME;
     private String DATABASE_PASSWORD;
 
-    public DBUtil(String username, String password, String url) {
-        this.DATABASE_URL = url;
+    public static DBUtil getInstance() {
+        return instance;
+    }
+
+    public void set(String hostname, String username, String password) {
+        this.DATABASE_HOSTNAME = "jdbc:mysql://" + hostname;
         this.DATABASE_USERNAME = username;
         this.DATABASE_PASSWORD = password;
     }
 
-    public DBUtil(String username, String password) {
-        this.DATABASE_URL = "45.76.5.38";
-        this.DATABASE_USERNAME = username;
-        this.DATABASE_PASSWORD = password;
+    public void connect() throws SQLException{
+        connection = DriverManager.getConnection(DATABASE_HOSTNAME, DATABASE_USERNAME, DATABASE_PASSWORD);
     }
+
 
     public void test() {
         String query = "SELECT fname, lname FROM Employee";
         try {
-            Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
+            Connection connection = DriverManager.getConnection(DATABASE_HOSTNAME, DATABASE_USERNAME, DATABASE_PASSWORD);
             Statement statement = connection.createStatement();
         } catch (SQLException e) {
-            System.out.println(e);
+            //System.out.println(e);
         }
 
     }
