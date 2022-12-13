60a0adc86f81252865eabfc65d5438404ab9062e
ryan <ryantiffany3@gmail.com>
2022-12-13 00:39:43 -0500

    edit database view and controller

--- src/main/java/com/rentaldb/controller/DatabaseController.java
+++ src/main/java/com/rentaldb/controller/DatabaseController.java
@@ -1,4 +1,54 @@
 package com.rentaldb.controller;
 
+import com.rentaldb.table.Equipment;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class DatabaseController {
+
+    @FXML
+    private TableView equipmentTable;
+    @FXML
+    private TableColumn<Equipment, Integer> equipIdCol;
+
+    @FXML
+    private TableColumn<Equipment, Boolean> equipIsAvailableCol;
+
+    @FXML
+    private TableColumn<Equipment, String> equipMakeCol;
+
+    @FXML
+    private TableColumn<Equipment, String> equipModelCol;
+
+    @FXML
+    private TableColumn<Equipment, Double> equipRateCol;
+
+    @FXML
+    private void initialize() {
+        equipIdCol.setCellValueFactory(cellData -> cellData.getValue().idProperty().asObject());
+        equipRateCol.setCellValueFactory(cellData -> cellData.getValue().rateProperty().asObject());
+        equipModelCol.setCellValueFactory(cellData -> cellData.getValue().modelProperty());
+        equipMakeCol.setCellValueFactory(cellData -> cellData.getValue().makeProperty());
+        equipIsAvailableCol.setCellValueFactory(cellData -> cellData.getValue().isAvailableProperty());
+    }
+
+    private void populateEquipment(Equipment equip) throws ClassNotFoundException{
+        //Declare and ObservableList for table view
+        ObservableList<Equipment> empData = FXCollections.observableArrayList();
+        //Add equipment to the ObservableList
+        empData.add(equip);
+        //Set items to the equipmentTable
+        equipmentTable.setItems(empData);
+    }
+
+
+
+
+
+
+
+
 }
