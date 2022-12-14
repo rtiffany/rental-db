376944a8cd33c4f2df4de39d68d0f7681335859e
ryan <ryan@ryan>
2022-12-14 12:46:21 -0500

    changed gui

--- src/main/java/com/rentaldb/controller/EmployeeController.java
+++ src/main/java/com/rentaldb/controller/EmployeeController.java
@@ -1,5 +1,65 @@
 package com.rentaldb.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class EmployeeController {
+
+    @FXML
+    private TableColumn<?, ?> employeeDotStatusCol;
+
+    @FXML
+    private TextField employeeDotStatusField;
+
+    @FXML
+    private TableColumn<?, ?> employeeFnameCol;
+
+    @FXML
+    private TextField employeeFnameField;
+
+    @FXML
+    private TableColumn<?, ?> employeeIdCol;
+
+    @FXML
+    private TextField employeeIdField;
+
+    @FXML
+    private TableColumn<?, ?> employeeLicCol;
+
+    @FXML
+    private TextField employeeLicNumField;
+
+    @FXML
+    private TableColumn<?, ?> employeeLnameCol;
+
+    @FXML
+    private TextField employeeLnameField;
+
+    @FXML
+    private TableColumn<?, ?> employeeSSNCol;
+
+    @FXML
+    private TextField employeeSSNField;
+
+    @FXML
+    private Button employeeSearchButton;
+
+    @FXML
+    private TextField employeeSearchField;
+
+    @FXML
+    private TableView<?> employeeTable;
+
+    @FXML
+    private Button showAllButton;
+
+    @FXML
+    private Button showAvailableButton;
+
+    @FXML
+    private Button showUnavailableButton;
 
 }
--- src/main/java/com/rentaldb/controller/EquipmentController.java
+++ src/main/java/com/rentaldb/controller/EquipmentController.java
@@ -1,21 +1,50 @@
 package com.rentaldb.controller;
 
 import com.rentaldb.table.Equipment;
+
+import com.rentaldb.table.EquipmentDAO;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
-import java.nio.Buffer;
+import java.sql.SQLException;
 
 public class EquipmentController {
 
     @FXML
+    private TextField equipmentIdField;
+
+    @FXML
+    private TextField equipmentIsAvailField;
+
+    @FXML
+    private TextField equipmentMakeField;
+
+    @FXML
+    private TextField equipmentModelField;
+
+    @FXML
+    private TextField equipmentRateField;
+
+    @FXML
+    private Button equipmentSearchButton;
+
+    @FXML
+    private TextField equipmentSearchField;
+
+    @FXML
     private Button showAllButton;
+
     @FXML
     private Button showAvailableButton;
+
+    @FXML
+    private Button showUnavailableButton;
 
     // Table
     @FXML
@@ -23,7 +52,7 @@ public class EquipmentController {
     @FXML
     private TableColumn<Equipment, Integer> equipIdCol;
     @FXML
-    private TableColumn<Equipment, Boolean> equipIsAvailableCol;
+    private TableColumn<Equipment, Boolean> equipIsAvailCol;
     @FXML
     private TableColumn<Equipment, String> equipMakeCol;
     @FXML
@@ -37,15 +66,35 @@ public class EquipmentController {
         equipRateCol.setCellValueFactory(cellData -> cellData.getValue().rateProperty().asObject());
         equipModelCol.setCellValueFactory(cellData -> cellData.getValue().modelProperty());
         equipMakeCol.setCellValueFactory(cellData -> cellData.getValue().makeProperty());
-        equipIsAvailableCol.setCellValueFactory(cellData -> cellData.getValue().isAvailableProperty());
+        equipIsAvailCol.setCellValueFactory(cellData -> cellData.getValue().isAvailableProperty());
+    }
+
+    @FXML
+    private void searchEquipment (ActionEvent actionEvent) throws ClassNotFoundException, SQLException {
+
+    }
+
+    @FXML
+    private void showAllEquipment (ActionEvent actionEvent) throws ClassNotFoundException, SQLException {
+        try {
+            ObservableList<Equipment> equipData = EquipmentDAO.showAllEquipment();
+            populateEquipments(equipData);
+        } catch (SQLException e) {
+            System.out.println("sql error: " + e);
+            throw e;
+        }
     }
 
     private void populateEquipment(Equipment equip) throws ClassNotFoundException{
         //Declare and ObservableList for table view
-        ObservableList<Equipment> empData = FXCollections.observableArrayList();
+        ObservableList<Equipment> equipData = FXCollections.observableArrayList();
         //Add equipment to the ObservableList
-        empData.add(equip);
+        equipData.add(equip);
         //Set items to the equipmentTable
-        equipmentTable.setItems(empData);
+        equipmentTable.setItems(equipData);
+    }
+
+    private void populateEquipments (ObservableList<Equipment> equipData) throws ClassNotFoundException {
+        equipmentTable.setItems(equipData);
     }
 }
